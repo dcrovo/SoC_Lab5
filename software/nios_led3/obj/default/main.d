212be517216c2d4d5630fa81f943c8ba6fea2ccf
Daniel Crovo <danielcrovop@gmail.com>
2022-11-09 15:46:57 -0500

    C sw DONE, TODO-Fibonacci

--- software/nios_led3/obj/default/main.d
+++ software/nios_led3/obj/default/main.d
@@ -1,10 +1,16 @@
 obj/default/main.o: main.c ../nios_led3_bsp//HAL/inc/alt_types.h \
- ../nios_led3_bsp/system.h ../nios_led3_bsp/linker.h gpio.h \
+ ../nios_led3_bsp/system.h ../nios_led3_bsp/linker.h \
+ ../nios_led3_bsp//HAL/inc/sys/alt_alarm.h \
+ ../nios_led3_bsp//HAL/inc/sys/alt_llist.h \
+ ../nios_led3_bsp//HAL/inc/priv/alt_alarm.h gpio.h \
  ../nios_led3_bsp//HAL/inc/io.h ../nios_led3_bsp//HAL/inc/alt_types.h \
  ../nios_led3_bsp//drivers/inc/altera_avalon_pio_regs.h
 ../nios_led3_bsp//HAL/inc/alt_types.h:
 ../nios_led3_bsp/system.h:
 ../nios_led3_bsp/linker.h:
+../nios_led3_bsp//HAL/inc/sys/alt_alarm.h:
+../nios_led3_bsp//HAL/inc/sys/alt_llist.h:
+../nios_led3_bsp//HAL/inc/priv/alt_alarm.h:
 gpio.h:
 ../nios_led3_bsp//HAL/inc/io.h:
 ../nios_led3_bsp//HAL/inc/alt_types.h:
